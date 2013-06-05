02f13da9c9a135a72e16ed85d3081843d7fff620
Tim Abell <tim@timwise.co.uk>
2013-06-05 21:45:03 +0100

    Test the first move in the first example play, failing build/test - Red

--- amazing-game-tests/GameEngineTests.cs
+++ amazing-game-tests/GameEngineTests.cs
@@ -29,6 +29,16 @@ namespace amazinggametests
 			Assert.AreEqual(4, game.MaxX, "incorrect board size on X-axis");
 			Assert.AreEqual(4, game.MaxY, "incorrect board size on Y-axis");
 		}
+
+		[Test()]
+		public void TestPattern1_Move1()
+		{
+			// check first move puts player in position 0,1 (because facing North)
+			var game = new Game();
+			game.ExecutePlayerCommand(PlayerCommand.Move);
+			Assert.AreEqual(0, game.Player.x);
+			Assert.AreEqual(1, game.Player.y);
+		}
 	}
 }
 
