bc25eb3bfe674dc201d220bac889f0b3ba3cf74d
Tim Abell <tim@timwise.co.uk>
2013-06-05 20:28:16 +0100

    New Game class for the engine

---
+++ amazing-game/Game.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace amazinggame
+{
+	/***
+	 * This is the engine for the game,
+	 * encapsulating all the rules of play
+	 * and the interactions the player can use.
+	 */
+	public class Game
+	{
+	}
+}
