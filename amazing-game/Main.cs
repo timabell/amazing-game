b501f7e41cc1bd78cc751d79362b05ce9fd259a7
Tim Abell <tim@timwise.co.uk>
2013-06-06 00:08:39 +0100

    Control info to screen, keys don't work yet

--- amazing-game/Main.cs
+++ amazing-game/Main.cs
@@ -8,6 +8,9 @@ namespace amazinggame
 		{
 			Console.Clear();
 			var game = new Game();
+			Console.Out.WriteLine("Amazing Game!! 2013, Tim Abell");
+			Console.Out.WriteLine("Controls: M - move, L - turn left, R - turn right, Q - quit.");
+			Console.Out.WriteLine();
 			Console.Out.WriteLine(game);
 		}
 	}
