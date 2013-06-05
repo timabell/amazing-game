562df76a29edb3037e61ba60fdff53a04f220838
Tim Abell <tim@timwise.co.uk>
2013-06-05 19:43:35 +0100

    Added default test file

---
+++ amazing-game-tests/GameEngineTests.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace amazinggametests
+{
+	[TestFixture()]
+	public class GameEngineTests
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
