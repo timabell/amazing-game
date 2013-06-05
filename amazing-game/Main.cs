1411231fe2573ea7f340c41e05b537975e7bbb49
Tim Abell <tim@timwise.co.uk>
2013-06-06 00:20:07 +0100

    Implemented key reading and game command execution.

--- amazing-game/Main.cs
+++ amazing-game/Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace amazinggame
 {
@@ -6,12 +7,43 @@ namespace amazinggame
 	{
 		public static void Main (string[] args)
 		{
-			Console.Clear();
 			var game = new Game();
-			Console.Out.WriteLine("Amazing Game!! 2013, Tim Abell");
-			Console.Out.WriteLine("Controls: M - move, L - turn left, R - turn right, Q - quit.");
-			Console.Out.WriteLine();
-			Console.Out.WriteLine(game);
+			char command = ' ';
+			var history = new StringBuilder();
+			while (command != 'q' && command != 'Q') {
+				// redraw
+				// TODO: overwrite chars instead to avoid flickering.
+				Console.Clear();
+				Console.Out.WriteLine("Amazing Game!! 2013, Tim Abell");
+				Console.Out.WriteLine("Controls: M - move, L - turn left, R - turn right, Q - quit.");
+				Console.Out.WriteLine();
+				Console.Out.WriteLine(game);
+				Console.Out.WriteLine();
+
+				// wait for user command
+				Console.Out.Write("Enter command: ");
+				Console.Out.Write(history);
+				var key = Console.ReadKey();
+				command = key.KeyChar;
+				history.Append(command);
+
+				// make a move, ignore irrelevant keystrokes
+				// TODO: use CompareTo() to cope with international keys
+				switch (command){
+				case 'm':
+				case 'M':
+					game.ExecutePlayerCommand(PlayerCommand.Move);
+					break;
+				case 'l':
+				case 'L':
+					game.ExecutePlayerCommand(PlayerCommand.Left);
+					break;
+				case 'r':
+				case 'R':
+					game.ExecutePlayerCommand(PlayerCommand.Right);
+					break;
+				}
+			}
 		}
 	}
 }
